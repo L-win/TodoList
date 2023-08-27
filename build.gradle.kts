f1c1e92bd0c74c519b3d76330c9c32e5fb6a9ed8
Elvin Agha <33497735+L-win@users.noreply.github.com>
2023-08-28 00:07:45 +0400

    Setup dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,18 @@
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.dagger.hilt.android") version "2.46.1" apply false
+//    id("org.jetbrains.kotlin.android.extensions") version "1.9.10"
 }
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val nav_version = "2.7.1"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
+
+
