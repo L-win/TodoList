4825625c7d7fa1f87ee1789763cccb6f018c71b6
Elvin Agha <33497735+L-win@users.noreply.github.com>
2023-08-31 20:54:50 +0400

    Setup dependency injection

--- app/src/main/java/com/elvina/todolist/data/TaskDatabase.kt
+++ app/src/main/java/com/elvina/todolist/data/TaskDatabase.kt
@@ -3,6 +3,7 @@ package com.elvina.todolist.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
+import com.elvina.todolist.di.ApplicationScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import javax.inject.Inject
